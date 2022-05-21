c6747df6ad5485a7355e50e44277b0deffcbed74
Venkata Jagannath <venky_jagannath@hotmail.com>
2022-05-21 11:44:54 -0500

    Added ARM template for the ADF factory create

---
+++ 1004-Create-Azure-data-factory.ps1
@@ -0,0 +1,24 @@
+######################################################################
+##                PART 4: Create a ADF instance #
+######################################################################
+
+####Venky Notes
+#1. Login in to cloud academy and create an azure sandbox. 
+#2. Login to the portal with the username and password that is given
+#3. Get the resource group name that has been provisioned. 
+  
+# DEFINE RESOURCE GROUP NAME AND LOCATION PARAMETERS
+$resourceGroupName = "1-b4d662d1-playground-sandbox"
+
+#Login to Azure  - First pass uncomment to login to azure.
+# Connect-AzAccount
+
+#################################01 - Create ADF ########################
+# Use ARM template to deploy resources
+Write-Host "Creating Azure Data Factory. This may take some time..."
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+  -TemplateFile "arm-templates/azure-data-factory/azuredeploy.json" `
+  -TemplateParameterFile "arm-templates/azure-data-factory/azuredeploy.parameters.json" `
+  -Mode Incremental `
+  -Force
