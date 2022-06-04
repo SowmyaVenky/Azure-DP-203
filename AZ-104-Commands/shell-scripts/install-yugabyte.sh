0bd9c71a8eb3f5c523121c91e7c7d610cc9c1c67
Venkata Jagannath <venky_jagannath@hotmail.com>
2022-06-04 16:11:35 -0500

    Adjusted yugabyte to python3

--- AZ-104-Commands/shell-scripts/install-yugabyte.sh
+++ AZ-104-Commands/shell-scripts/install-yugabyte.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install -y openjdk-17-jdk
-sudo apt install -y python
+sudo apt install -y python-is-python3
 wget https://downloads.yugabyte.com/releases/2.13.2.0/yugabyte-2.13.2.0-b135-linux-x86_64.tar.gz
 tar xvfz yugabyte-2.13.2.0-b135-linux-x86_64.tar.gz && cd yugabyte-2.13.2.0/
 ./bin/post_install.sh
