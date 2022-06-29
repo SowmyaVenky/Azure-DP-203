537ecd4cbf8e895cfe8950dad5f4669e0fb3fef0
Venkata Jagannath <venky_jagannath@hotmail.com>
2022-06-29 14:07:54 -0500

    Tested file creates on DSVM

--- SparkExamples/README.md
+++ SparkExamples/README.md
@@ -2,9 +2,21 @@ set JAVA_HOME=c:\Venky\jdk-11.0.15.10-hotspot
 set PATH=%PATH%;c:\Venky\spark\bin;c:\Venky\apache-maven-3.8.6\bin
 set SPARK_HOME=c:\Venky\spark
 
+## Computer-2
+set PATH=c:\Venky\spark\bin;c:\Venky\apache-maven-3.8.4\bin;%PATH%
+
+
 cd C:\Venky\DP-203\SowmyaVenkyRepo\Azure-DP-203\SparkExamples
 mvn clean package
 
-SET HADOOP_HOME=C:\Venky\DP-203\SowmyaVenkyRepo\Azure-DP-203\SparkExamples
-spark-submit --master local[4] --class com.gssystems.spark.MovieLensProcessing target\SparkExamples-1.0-SNAPSHOT.jar
+SET HADOOP_HOME=C:\Venky\DP-203\Azure-DP-203\SparkExamples
 
+## Computer-2 
+SET HADOOP_HOME=C:\Venky\DP-203\SowmyaVenkyRepo\Azure-DP-203\SparkExamples
+
+spark-submit --master local[4] --class com.gssystems.spark.MovieLensProcessing target\SparkExamples-1.0-SNAPSHOT.jar file:///C:/Venky/DP-203/SowmyaVenkyRepo/movielens/movies_metadata.csv file:///C:/Venky/DP-203/SowmyaVenkyRepo/movielens/ratings.csv
+
+## On DSVM
+rm -rf genre movie_genre
+
+spark-submit --master local[4] --class com.gssystems.spark.MovieLensProcessing SparkExamples-1.0-SNAPSHOT.jar file:///home/venkyuser/SowmyaVenkyRepo/movielens/movies_metadata.csv file:///home/venkyuser/SowmyaVenkyRepo/movielens/ratings.csv
--- SparkExamples/src/main/java/com/gssystems/spark/MovieLensProcessing.java
+++ SparkExamples/src/main/java/com/gssystems/spark/MovieLensProcessing.java
@@ -2,8 +2,10 @@ package com.gssystems.spark;
 
 import com.google.gson.Gson;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.Function3;
 import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
@@ -16,11 +18,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MovieLensProcessing {
     public static void main(String[] args) {
-        String moviesFile = "C:\\Venky\\DP-203\\SowmyaVenkyRepo\\movielens\\movies_metadata.csv";
-        String ratingsFile = "C:\\Venky\\DP-203\\SowmyaVenkyRepo\\movielens\\ratings.csv";
+    	if( args == null || args.length != 2 ) {
+    		System.out.println("Need to pass 2 parameters - movies_metadata.csv and ratings.csv for this to work!");
+    		System.exit(-1);
+    	}
+    	
+        String moviesFile = args[0]; 
+        String ratingsFile = args[1];
         SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
 
@@ -43,7 +51,7 @@ public class MovieLensProcessing {
         System.out.println(movies_df.count());
         movies_df.show();
 
-        // Maps to apr_item_number=optimized price json
+        // Maps genere_id = genre_name
         PairFlatMapFunction<Row, Integer, String> genremapper = new PairFlatMapFunction<Row, Integer, String>() {
             private static final long serialVersionUID = 1L;
 
@@ -51,6 +59,7 @@ public class MovieLensProcessing {
             @Override
             public Iterator<Tuple2<Integer, String>> call(Row t) throws Exception {
                 Gson gson = new Gson();
+                
                 List<Tuple2<Integer, String>> toRet = new ArrayList<Tuple2<Integer, String>>();
                 //Get json array of genres.
                 String genrejson = t.getString(0);
@@ -59,9 +68,10 @@ public class MovieLensProcessing {
                     if( genereParsed.size() > 0 ) {
                         for(Map<String, Object> m : genereParsed) {
                             //System.out.println(m);
-                            Double idval = Double.parseDouble(m.get("id").toString());
+                            Double idval = Double.parseDouble(m.get("id").toString());   
+                            m.put("id", idval.intValue());                          
                             Tuple2<Integer, String> pair = new Tuple2<Integer, String>(
-                                    idval.intValue(), m.get("name").toString());
+                                    idval.intValue(), gson.toJson(m));
                             toRet.add(pair);
                         }
                     }
@@ -71,32 +81,113 @@ public class MovieLensProcessing {
         };
 
         //Calculate genre
-        JavaPairRDD<String, String> genrerdd = moviesdf.select("genres").toJavaRDD().flatMapToPair(genremapper);
+        JavaPairRDD<Integer, String> genrerdd = moviesdf.select("genres").toJavaRDD().flatMapToPair(genremapper);
         System.out.println("Genre count =" + genrerdd.count());
 
-        //de-dup the values
-        Function3<Integer, String, Integer, String> recordstorededupper = new Function3<Integer, String, Integer, String>() {
-            @Override
-            public String call(Integer integer, String s, Integer integer2) throws Exception {
-                return null;
+        //Reducer to de-duplicate the genres
+        Function2<String, String, String> genreReducer = new Function2<String, String, String>() {
+			private static final long serialVersionUID = -6061831643810838526L;
+
+			@Override
+			public String call(String v1, String v2) throws Exception {
+				//return one of them.
+				//System.out.println(v1);
+				return v1;
+			}        	
+        };
+		JavaPairRDD<Integer,String> genreReduced = genrerdd.reduceByKey(genreReducer);
+		System.out.println("genreReduced count =" + genreReduced.count());  
+
+		Function<Tuple2<Integer, String>, String> removeKeyFunction = new Function<Tuple2<Integer, String>, String>() {
+			private static final long serialVersionUID = -4135265152684593116L;
+
+			@Override
+			public String call(Tuple2<Integer, String> v1) throws Exception {
+				// TODO Auto-generated method stub
+				return v1._2();
+			}
+			
+		};
+
+		//Remove key and just use json
+		JavaRDD<String> genrekeysremoved = genreReduced.map(removeKeyFunction);	
+		System.out.println("Writing genre master file...");
+		genrekeysremoved.repartition(1).saveAsTextFile("genre");		
+		
+		
+		//Now map movie_id to genres and de-dup that 
+        // Maps movie_id = genre_id
+        PairFlatMapFunction<Row, Integer, String> movie_id_to_genre_id_mapper = new PairFlatMapFunction<Row, Integer, String>() {
+            private static final long serialVersionUID = 1L;
+
+            private boolean isNumeric(String strNum) {
+                if (strNum == null) {
+                    return false;
+                }
+                try {
+                    double d = Double.parseDouble(strNum);
+                } catch (NumberFormatException nfe) {
+                    return false;
+                }
+                return true;
             }
-
-            private static final long serialVersionUID = -3062191337438821870L;
-
-            @SuppressWarnings({ "unchecked", "rawtypes" })
+            
+            @SuppressWarnings({ "rawtypes" })
             @Override
-            public String call(Integer v1, String v2) throws Exception {
-                Map valsConsolidated = new TreeMap();
-
-                // We consolidate both maps and push the de-dup list of vehicles.
-                valsConsolidated.putAll(vals1);
-                valsConsolidated.putAll(vals2);
-
-                String mergedJson = gson.toJson(valsConsolidated);
-                return mergedJson;
+            public Iterator<Tuple2<Integer, String>> call(Row t) throws Exception {
+                Gson gson = new Gson();
+                List<Tuple2<Integer, String>> toRet = new ArrayList<Tuple2<Integer, String>>();
+                //Get json array of genres.
+                String genrejson = t.getString(1);
+                String movieId = t.getString(0);
+                
+                //System.out.println("movieId = " + movieId + ", genre = " + genrejson);
+                Map<String, Object> emit = new TreeMap<String, Object>();
+                if( isNumeric(movieId)) {
+                	Double movieidDouble = Double.parseDouble(movieId);
+                    
+                    if( genrejson != null && genrejson.trim().length() > 0  && genrejson.trim().startsWith("[")) {
+                        List<Map<String, Object>> genereParsed = gson.fromJson(genrejson, List.class);
+                        if( genereParsed.size() > 0 ) {
+                            for(Map<String, Object> m : genereParsed) {
+                            	emit.clear();
+                                Double idval = Double.parseDouble(m.get("id").toString());
+                                emit.put("movie_id", movieidDouble.intValue());
+                                emit.put("genre_id", idval.intValue());
+                                
+                                String emitJson = gson.toJson(emit);
+                                
+                                Tuple2<Integer, String> pair = new Tuple2<Integer, String>(
+                                		movieidDouble.intValue(), emitJson);
+                                toRet.add(pair);
+                            }
+                        }
+                    }                  
+                }
+                
+                return toRet.iterator();
             }
         };
 
+        //Calculate movie_id_genre_id
+        JavaPairRDD<Integer, String> movieidtogenreidRdd = moviesdf.select("id", "genres").toJavaRDD().flatMapToPair(movie_id_to_genre_id_mapper);
+        System.out.println("movieidtogenreidRdd count =" + movieidtogenreidRdd.count());
+
+		Function<Tuple2<Integer, String>,String> removeKeyFunction1 = new Function<Tuple2<Integer, String>,String>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public String call(Tuple2<Integer, String> v1) throws Exception {
+				return v1._2();
+			}
+			
+		};
+		
+		//Remove key and just use json
+		JavaRDD<String> movieGenreJson = movieidtogenreidRdd.map(removeKeyFunction1 );	
+		System.out.println("Writing movie_genre file...");
+		movieGenreJson.repartition(1).saveAsTextFile("movie_genre");		
+		
         Dataset ratingsdf = spark.read().option("header","true").csv(ratingsFile);
         ratingsdf = ratingsdf.select(
             ratingsdf.col("userId").alias("user_id"),
