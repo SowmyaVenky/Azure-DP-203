78b4cbe4f5c4ebd1e8c3d3f0ba04e92fd7ed8c0a
Venkata Jagannath <venky_jagannath@hotmail.com>
2023-02-01 17:01:46 -0600

    Added chat GPT integration to the client app

--- PaymentsHackathon/teamsapp/app.js
+++ PaymentsHackathon/teamsapp/app.js
@@ -13,6 +13,13 @@ const unirest = require('unirest');
 const crypto = require('crypto');
 const setTimeOut = require('timers/promises');
 
+//Venky added this for chatgpt integration, 
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 //For RTP
 var api_key = process.env.KEY;
 var api_secret = process.env.SECRET;
@@ -30,7 +37,11 @@ app.use(session({
 }));
 app.set('trust proxy', 1);
 
-app.use(cors());
+//Venky added this to make it easy for chatgpt integration.
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
@@ -46,10 +57,10 @@ const {
 
 // Create connection
 const db = mysql.createConnection({
-  host: "localhost",  
-  user: "root",  
-  //host: "autorepairdb1001.mysql.database.azure.com",  
-  //user: "autoadmin",  
+  //host: "localhost",  
+  //user: "root",  
+  host: "autorepairdb1001.mysql.database.azure.com",  
+  user: "autoadmin",  
   password: "Ganesh20022002"
 });
 
@@ -61,6 +72,29 @@ db.connect((err) => {
     console.log("MySql Connected");
   });
 
+app.post('/chatgpt', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+  
 
 app.get('/', function(req, res) {
     var loginerror = req.query.loginerror;
@@ -82,16 +116,37 @@ app.get('/', function(req, res) {
 });
 
 app.get('/records', function(req, res, next) {
-  console.log("hit records call");
+  //console.log("hit records call");
     var email = req.query.email
     var password = req.query.password
 
     var params = [email.trim().toUpperCase(), password.trim().toUpperCase()];
 
-    console.log("parms " + params);
+    //console.log("parms " + params);
     let sql =  
-      "select SR.customer_id, SR.id as id, E.name, SR.vehicle_color, SR.vehicle_vin, SR.vehicle_vrm, SR.vehicle_manufacturer, " +
-      "SR.vehicle_model, SR.vehicle_problem, SR.vehicle_repair_cost, SR.vehicle_repair_date, SR.vehicle_repair_status " +
+      "select SR.customer_id, " +
+      "SR.id as id, " + 
+      "E.name, " + 
+      "C.firstname, " + 
+      "C.lastname, " + 
+      "C.title, " + 
+      "C.phone, " + 
+      "C.email, " + 
+      "C.address, " +
+      "C.address2, " +
+      "C.city, " +  
+      "C.state, " +  
+      "C.zip, " + 
+      "C.creditcard, " + 
+      "SR.vehicle_color, " +
+      "SR.vehicle_vin, " + 
+      "SR.vehicle_vrm, " + 
+      "SR.vehicle_manufacturer, " +
+      "SR.vehicle_model, " + 
+      "SR.vehicle_problem, " + 
+      "SR.vehicle_repair_cost, " + 
+      "SR.vehicle_repair_date, " + 
+      "SR.vehicle_repair_status " +
       "from " +
       "autorepair.customer C " +
       "LEFT JOIN " +
@@ -116,7 +171,7 @@ app.get('/records', function(req, res, next) {
     var vehicleVin = new Set();
     
     db.query(sql, params, (err, rows) => {
-        console.log("Here's your rows: " + err);
+        //console.log("Here's your rows: " + err);
         var numRows = rows.length;        
         if(err) throw err;  
         if( numRows == 0 ) {
